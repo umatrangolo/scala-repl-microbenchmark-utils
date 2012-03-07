04953d99de2cf009a51e00f9963fa1bcc64d42f0
Ugo Matrangolo <umatrangolo@gilt.com>
2012-03-07 15:20:24 +0000

    First working version with nice TimeUnit conversion

--- src/main/scala/com/umatrangolo/microbenchmark/utils/Benchmarks.scala
+++ src/main/scala/com/umatrangolo/microbenchmark/utils/Benchmarks.scala
@@ -1,19 +1,25 @@
 package com.umatrangolo.repl.microbenchmark 
 
 /** Simple logger for the REPL. */
-object Logger {
+object Logger { // TODO replace with Console
   def log(msg: String) { println("[%s] -- %s ".format(new java.util.Date, msg)) }
 }
 
-/** This encapsulates the timings of a test run. */
-case class Report(name: Option[String] = None, var times: List[Long]) {
+/** This encapsulates the timings of a test run.
+  *
+  * @param name identifier of this test outcome
+  * @param time an array of ''Long'' with the times taken by each run of the test.
+  */
+case class Outcome(name: Option[String] = None, var times: List[Long]) {
   
+  /** Represents a single time outcome of a test run
+    *
+    * @param time a ''Long'' with the time in ns (default time unit)
+    */
   case class Timing(time: Long) {
-    import java.util.concurrent.TimeUnit
-
-    val inMs: Long = TimeUnit.MILLISECONDS.convert(time, TimeUnit.NANOSECONDS)
-    val inSecs: Long = TimeUnit.SECONDS.convert(time, TimeUnit.NANOSECONDS)
-    val inNs: Long = time  // default time unit is nanosecs
+    private[microbenchmark] val ms: Long = TimeUnit.Milliseconds(time)
+    private[microbenchmark] val s: Long = TimeUnit.Seconds(time)
+    private[microbenchmark] val ns: Long = time  // default time unit is nanosecs
 
     override def toString = "%d".format(time)
   }
@@ -22,23 +28,53 @@ case class Report(name: Option[String] = None, var times: List[Long]) {
   val max = Timing(times map { o => o } max)
   val min = Timing(times map { o => o } min)
   val all = times map { Timing(_) }  
+}
 
-  private def tabulate(avg: Long, max: Long, min: Long, unit: String): String = 
+/** Time units supported by the benchmark utils.
+  *
+  * All the test runs are measured using the ns time unit. Each TimeUnit instance
+  * defines an '''apply()''' method to convert from ns to one of the other supported
+  * units.
+  *
+  * e.g.: TimeUnit.Seconds(123456789l) will convert the given ns in s.
+  */ 
+object TimeUnit extends Enumeration {
+  import java.util.concurrent.{ TimeUnit => JTimeUnit }
+
+  case class TimeUnitValue(sid: String, conv: (Long) => Long) extends Val {
+    def apply(ns: Long): Long = conv(ns)
+  }
+
+  type TimeUnit = TimeUnitValue // path dependant type --> Scala way of impl 'enumeration'
+ 
+  val Seconds = new TimeUnitValue("s", JTimeUnit.SECONDS.convert(_, JTimeUnit.NANOSECONDS))
+  val Milliseconds = new TimeUnitValue("ms" ,JTimeUnit.MILLISECONDS.convert(_, JTimeUnit.NANOSECONDS)) 
+  val Nanoseconds = new TimeUnitValue("ns", { ns => ns }) 
+}
+
+import TimeUnit._
+
+case class Summary(outcome: Outcome, timeUnit: TimeUnit = TimeUnit.Nanoseconds) {
+
+  def timeUnit(unit: TimeUnit): Summary = this.copy(timeUnit = unit)
+
+  private def tabulate: String = 
     """
      | Test: %s
      | ------------------------------
      | Avg time: %s %s
      | Max time: %s %s
      | Min time: %s %s
-    """.format(name.getOrElse("*"), avg, unit, max, unit, min, unit).stripMargin
+    """.format(outcome.name.getOrElse("?"), 
+	       timeUnit(outcome.avg.ns), timeUnit.sid, 
+	       timeUnit(outcome.max.ns), timeUnit.sid,
+	       timeUnit(outcome.min.ns), timeUnit.sid).stripMargin
 
-  override def toString = tabulate(avg.inNs, max.inNs, min.inNs, "ns.")
-  def inSecs = tabulate(avg.inSecs, max.inSecs, min.inSecs, "s.")
-  def inMs = tabulate(avg.inMs, max.inMs, min.inMs, "ms.")
+  override def toString = tabulate
 }
 
-/**  Main object that provides the time() method to time a series of n executions
-  *  of the code that we are micro benchmarking.
+/** Main object that provides the time() method to time benchmark a series of n executions
+  * of a given function.
   */ 
 object Timer {
 
@@ -48,7 +84,7 @@ object Timer {
     * @param f function being tracked
     * @return a `Report` describing the observed behavior
     */  
-  private[microbenchmark] def time(n: Int, name: Option[String])(f: => Unit): Report = {
+  private[microbenchmark] def time(n: Int, name: Option[String])(f: => Unit): Outcome = {
     Logger.log("Starting %s ...".format(name.getOrElse("")))
     
     val times: List[Long] = List() ++ (0 until n) map { i =>
@@ -59,9 +95,9 @@ object Timer {
 
     Logger.log("Done.")
     
-    Report(name, times)
+    Outcome(name, times)
   }
 
-  def time(n: Int)(f: => Unit): Report = time(n, None) { f } 
-  def time(n: Int, name: String)(f: => Unit): Report = time(n, Some(name)) { f } 
+  def time(n: Int)(f: => Unit): Outcome = time(n, None) { f } 
+  def time(n: Int, name: String)(f: => Unit): Outcome = time(n, Some(name)) { f } 
 }
